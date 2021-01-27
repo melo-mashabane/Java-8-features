e3947c03f9aa90d35db239f5a66063864392ebbb
Melo Mashabane <melomashabane@192.168.1.101>
2021-01-27 15:53:38 +0000

    Clear previous demo data

--- src/com/iwalksoftware/demo/InterfaceTwo.java
+++
@@ -1,9 +0,0 @@
-package com.iwalksoftware.demo;
-
-public interface InterfaceTwo {
-    void methodOne();
-
-    default void log(String str) {
-        System.out.println(str + " Interface Two logging");
-    }
-}
