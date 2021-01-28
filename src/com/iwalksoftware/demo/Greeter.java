0723838770caf13ccc302fe499f9f036db8cee27
Melo Mashabane <melomashabane@192.168.1.101>
2021-01-28 08:16:51 +0000

    Lambda vs Interface implementations

--- src/com/iwalksoftware/demo/Greeter.java
+++ src/com/iwalksoftware/demo/Greeter.java
@@ -8,11 +8,21 @@ public class Greeter {
 
     public static void main(String[] args) {
         Greeter greeter = new Greeter();
-        HelloWorldGreeting helloWorldGreeting = new HelloWorldGreeting();
-        Greeting interfaceVariable = () -> System.out.println("Hello I am an iWalk Interface lambda!");
+        Greeting lambdaGreeting = () -> System.out.println("Hello I am an iWalk Interface lambda!" + "\n");
 
-        greeter.greet(helloWorldGreeting);
-        interfaceVariable.perform();
+        Greeting InnerClass = new Greeting(){
+
+            @Override
+            public void perform() {
+                System.out.println("Welcome to iWalk. From Inner class" + "\n" +
+                        " Both look similar but are not the same!" + "\n" +
+                        " They are just different ways of implementing an interface." + "\n" +
+                        " Lambda is not a way of creating an Inner Class. They have their own function!");
+            }
+        };
+
+
+        greeter.greet(lambdaGreeting);
+        greeter.greet(InnerClass);
     }
-
 }
