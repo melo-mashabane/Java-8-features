eae6ff880f69a96f313b323b2447c0f39b8097a0
Melo Mashabane <melomashabane@192.168.1.101>
2021-01-28 07:46:05 +0000

    Lambda vs Interface implementations

--- src/com/iwalksoftware/demo/Greeter.java
+++ src/com/iwalksoftware/demo/Greeter.java
@@ -9,24 +9,10 @@ public class Greeter {
     public static void main(String[] args){
         Greeter greeter = new Greeter();
         HelloWorldGreeting helloWorldGreeting = new HelloWorldGreeting();
-        greeter.greet(helloWorldGreeting);
-        helloWorldGreeting.perform();
-
-        myLambda someVariable = () -> System.out.println("Hello I am an iWalk lambda!");
-        anotherLambda anotherVariable = (String str) -> System.out.println("Hello " + str +", I am an iWalk lambda!");
         Greeting interfaceVariable = () -> System.out.println("Hello I am an iWalk Interface lambda!");
 
-        someVariable.method();
+        greeter.greet(helloWorldGreeting);
         interfaceVariable.perform();
-        anotherVariable.method("Melo");
-
     }
 
-    interface myLambda {
-       void method();
-    }
-
-    interface anotherLambda {
-        void method(String str);
-    }
 }
