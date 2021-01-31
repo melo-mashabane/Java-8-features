5d1da05ae61fc098f305e6ccbd83721f28d94cd4
melomashabane <mmashabane190291@gmail.com>
2021-01-31 19:18:58 +0000

    Add code reduction

--- src/com/iwalksoftware/demo/TypeInferenceDemo.java
+++ src/com/iwalksoftware/demo/TypeInferenceDemo.java
@@ -3,7 +3,7 @@ package com.iwalksoftware.demo;
 public class TypeInferenceDemo {
 
     public static void main(String[] args) {
-        StringLenghthLambda myLambda = (String str) -> str.length();
+        StringLenghthLambda myLambda = str -> str.length();
         System.out.println(myLambda.methodOne("Welcome to iWalk"));
     }
 
